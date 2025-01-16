b5eb5fd9c84c27afebd834a7b246a59a6057a258
Islam Atwa <Islam Atwa>
2025-01-17 01:41:26 +0200

    Add structure.md file

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Quiz App Loaded');
-});
+
+function getQuestions(){
+    let myRequest = new XMLHttpRequest();
+    myRequest.onreadystatechange = function(){
+        if (this.readyState === 4 && this.status === 200) {
+            console.log(this.responseText);
+        }
+        else{
+            console.log('Error');
+        }
+    };
+    myRequest.open("GET", "html-qustion.json", true);
+    myRequest.send();
+}
+getQuestions();
---
+++ structure.md
@@ -0,0 +1,14 @@
+-Quiz App
+-html
+    -head
+    -body
+        -quiz-app
+            -quiz-info
+            -quiz-area
+            -answer-area
+            -button
+            -bullets
+            -result
+            
+
+
