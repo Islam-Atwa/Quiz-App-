744fb1b138d9c52347cb34ff455c85915f81e45b
Islam Atwa <Islam Atwa>
2025-01-17 21:42:28 +0200

    add qustion data and create bullets and create function submitButton andmake some changes to the code

--- main.js
+++ main.js
@@ -1,15 +1,80 @@
+    // select elements
+    let countspan = document.querySelector(".quiz-info .count");
+    let bulletsSpanContainer = document.querySelector(".bullets .spans");
+    let quizArea = document.querySelector(".quiz-area");
+    let answerArea = document.querySelector(".answer-area");
+    let submitButton = document.querySelector(".submit-button"); // select the submit button
+
+    // set options   
+    let currentIndex = 0;
+    let rightAnswers = 0;
+     // تعريف qustion في النطاق العام
+
 
 function getQuestions(){
     let myRequest = new XMLHttpRequest();
-    myRequest.onreadystatechange = function(){
-        if (this.readyState === 4 && this.status === 200) {
-            console.log(this.responseText);
+    myRequest.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+        
+        let qustion = JSON.parse(this.responseText);
+        let qCount = qustion.length;
+        // console.log("Request Status:", this.status); // Add this line to check the status.
+        createBullets(qCount);
+        addQustionData(qustion[currentIndex], qCount);
+
+        // Click on Submit Button
+        submitButton.onclick = () => {
+            let theRightAnswer = qustion[currentIndex].right_answer;
+            console.log(theRightAnswer);
+            
         }
-        else{
-            console.log('Error');
-        }
-    };
-    myRequest.open("GET", "html-qustion.json", true);
-    myRequest.send();
+    }
+};
+
+myRequest.open("GET", "html-qustion.json", true);
+myRequest.send();
 }
 getQuestions();
+
+    // create bullets fn
+
+function createBullets(num){
+    countspan.innerHTML =  num;
+    // create bullets
+    
+}
+
+function addQustionData(obj, count){
+    
+    let qTitle = document.createElement("h2"); // create question title
+
+    let qText = document.createTextNode(obj['title']); // create question text
+
+    qTitle.appendChild(qText); // append text to title H2
+
+    quizArea.appendChild(qTitle); // append title to quiz area
+
+    for (let i =1; i <= 4; i++){
+        let mainDiv = document.createElement("div");  // create main div
+        mainDiv.className = "answer"; // add class to main div
+
+        let radioInput = document.createElement("input"); // create radio input
+        radioInput.name = "qustion"; // add name to radio input
+        radioInput.type = "radio"; // add type to radio input
+        radioInput.id = `answer${i}`;    // add id to radio input
+        radioInput.dataset.answer = obj[`answer${i}`]; // add data answer to radio input
+
+        let theLable = document.createElement("label"); // create label
+        theLable.htmlFor = `answer${i}`; // add for to label
+
+        let theLableText = document.createTextNode(obj[`answer${i}`]); // create text for label
+        theLable.appendChild(theLableText); // append text to label
+
+        mainDiv.appendChild(radioInput); // append radio input to main div
+        mainDiv.appendChild(theLable); // append lable to main div
+
+        // append all main divs to answer area 
+        answerArea.appendChild(mainDiv);
+    }   
+}
+
